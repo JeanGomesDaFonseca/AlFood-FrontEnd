a8a8071a84a9603b33a06e83a2ce5d8c4ff7aaba
JeanGomesDaFonseca <jgomesfonseca21@gmail.com>
2022-11-16 15:18:43 -0300

    commit 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './paginas/Home';
-import VitrineRestaurantes from './paginas/VitrineRestaurantes';
+import { Routes, Route } from "react-router-dom";
+import AdministracaoRestaurantes from "./paginas/Administracao/Restaurantes/AdministracaoRestaurantes";
+import Home from "./paginas/Home";
+import VitrineRestaurantes from "./paginas/VitrineRestaurantes";
 
 function App() {
-
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
+      <Route
+        path="/admin/restaurantes"
+        element={<AdministracaoRestaurantes />}
+      />
     </Routes>
   );
 }
---
+++ src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -0,0 +1,5 @@
+const AdministracaoRestaurantes = () => {
+  return <h1>Restaurantes</h1>;
+};
+
+export default AdministracaoRestaurantes;
